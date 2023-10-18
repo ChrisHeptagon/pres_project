573d77975f16e072569ebf7d9ae35ea2864f3bc6
ChrisHeptagon <ccollymore348@gmail.com>
2023-10-17 21:21:24 -0400

    Switched to Go

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -22,42 +20,24 @@ func main() {
 	app.Static("/assets", "./react/dist/assets/")
 	app.Static("/presentation/*", "./react/dist/")
 
-	app.Listen(":3900")
+	app.Listen(":5000")
 }
 
 func ServeMDX(app *fiber.App) error {
-	app.Get("/presentation-mdx/:slide", func(c *fiber.Ctx) error {
+	app.Get("/presentation-mdx", func(c *fiber.Ctx) error {
 		pwd, err := os.Getwd()
 		if err != nil {
 			return c.SendString("error: Could not get working directory")
 		}
 		fmt.Printf("pwd: %v\n", pwd)
-		filePath := fmt.Sprintf("%s/next/src/presentation.mdx", pwd)
+		filePath := fmt.Sprintf("%s/astro/presentation.mdx", pwd)
 		content, err := os.ReadFile(filePath)
 		if err != nil {
 			return c.SendString("error: File not found")
 		}
 		contentString := string(content)
 		fmt.Printf("contentString: %v\n", contentString)
-		splitString := strings.Split(contentString, "---")
-		fmt.Printf("splitString: %v\n", splitString)
-		slide := c.Params("slide")
-		slideNumber, err := strconv.Atoi(slide)
-		fmt.Printf("slide: %v\n", slideNumber)
-
-		if err != nil || slideNumber < 0 {
-			return c.SendString("error: Invalid slide number")
-		} else if slideNumber > len(splitString) || slideNumber < 1 {
-			return c.SendString("error: Slide not found")
-		}
-		slideNumber--
-
-		slideContent := splitString[slideNumber]
-		if slideContent == "" {
-			return c.SendString("error: Slide content not found")
-		}
-		fmt.Printf("slideContent: %v\n", slideContent)
-		return c.SendString(slideContent)
+		return c.SendString(contentString)
 	})
 	return nil
 }
